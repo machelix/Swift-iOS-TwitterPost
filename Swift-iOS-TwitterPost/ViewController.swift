f37527aa102cc527518530359ebd7e4ecefe1f90
rudiluis <rudi121_kemo@hotmail.com>
2015-02-13 12:03:58 +0000

    First commit

---
+++ Swift-iOS-TwitterPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-iOS-TwitterPost
+//
+//  Created by Rudi Luis on 13/02/15.
+//  Copyright (c) 2015 Rudi Luis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
